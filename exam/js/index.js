a24bb7213115e29583132838ade9dd84146a50c1
hiphop5782 <zdlsz@hanmail.net>
2023-04-04 10:41:08 +0900

    평가 안내 페이지 작성

---
+++ exam/js/index.js
@@ -0,0 +1,62 @@
+Vue.createApp({
+    data(){
+        return {
+            subject:{
+                title:"UI 디자인",
+                date:"2023-04-04",
+            },
+            subList:[
+                {no:1, type:"서술형", count:4, begin:"09:30", end:"11:30", progress:0, remain:0},
+                {no:2, type:"포트폴리오", count:1, begin:"09:30", end:"11:30", progress:0, remain:0},
+            ],
+            noticeList:[
+                "답안이 공백일 경우 0점 처리됩니다",
+                "평가 완료 전까지 이동할 수 없습니다",
+                "정해진 쉬는 시간 외에 다른 층으로 이동할 수 없습니다",
+                "질문이 있을 경우 강사에게 문의 바랍니다"
+            ],
+            now:moment(),
+            styles:{
+                clear:{"color":"gray"},
+                almost:{"color":"red"},
+                remain:{"color":"black"}
+            },
+        };
+    },
+    computed:{
+        nowFormat(){
+            return this.now.format("YYYY-MM-DD dddd HH:mm:ss");
+        },
+    },
+    watch:{
+        
+    },
+    methods:{
+        strFormat(str) {
+            return moment(str, "yyyy-MM-dd").format("y년 M월 d일 dddd");
+        },
+        timeFormat(value) {
+            if(value < 0) return "평가 종료";
+            return moment.utc(value * 1000).format('H시간 m분 s초');
+        }
+    },
+    created(){
+        this.now = moment();
+
+        setInterval(()=>{
+            this.now = moment();
+
+            this.subList.forEach((s)=>{
+                const beginObj = moment(s.begin, 'HH:mm');
+                const endObj = moment(s.end, 'HH:mm');
+                const examDuration = moment.duration(endObj.diff(beginObj));
+                const timeDuration = moment.duration(this.now.diff(beginObj));
+                const total = parseInt(examDuration.asSeconds());
+                const current = parseInt(timeDuration.asSeconds());
+                const percent = Math.max(Math.min(100, current * 100 / total), 0);
+                s.progress = percent;
+                s.remain = total - current;
+            });
+        }, 1000);
+    },
+}).mount("#app");
